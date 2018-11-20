78259769ac019448832b508018b28ae534cdb1cf
Pablo <Pablo@LAPTOP-UQ3S4M4Q>
2018-11-20 16:23:15 +0100

    Subida de carpeta CSS y pequeña modificación de la pagina de inicio

--- CentroMail/application/views/welcome_message.php
+++ CentroMail/application/views/welcome_message.php
@@ -2,103 +2,18 @@
 /**
  * @author Pablo Piedad Garrido
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');x
 ?><!DOCTYPE html>
 <html lang="es">
 <head>
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>CSS/estilosCSS.css">
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 25px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-        
-        ul{
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            background-color: #333;
-            
-        }
-        
-        li{
-            float:left;
-        }
-        
-        li a{
-            display:block;
-            color:white;
-            text-align:center;
-            padding: 14px 16px;
-            text-decoration: none;
-        }
-        
-        li a:hover{
-            background-color:#111;
-        }
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+	<title>CentroMail</title>
 </head>
 <body>
 
-<div id="container">
-    <center>
-	<h1>Pagina de inicio</h1>
-    </center>
+<div align="center" id="container">
+<h1>Pagina de inicio</h1>
 	<div id="body">
             <ul>
                 <li> <a href="<?=base_url().'index.php/Welcome/'?>" title="Inicio">Inicio</a></li>
@@ -106,13 +21,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <li style="float:right"><a href="#login">Login</a></li>
             </ul>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+            <p>Bienvenido a la web de CentroMail.<br><br>En ella encontrará valoraciones de videojuegos y comentarios de los usuarios<p>
+            <center>
+                <h2>VIDEOJUEGOS MÁS VALORADOS SEGÚN NUESTROS USUARIOS</h2>
+            </center>
+            <!--//Aqui iria una vista a videojuegos cuya valoración va de mayor a menor-->
 	</div>
 	
 </div>
