36232232abbb8f918965c29a0655b2239148f801
sheratan <sheratan@sheratan-GE70-2PE>
2018-11-20 15:33:44 +0100

    Usuario sin terminar y clave de encriptación

---
+++ CentroMail/application/views/usuario_view.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Articulos</title>
+    </head>
+    <body>
+        <h1>Bienvenido a la web sobre artículos</h1>
+        <a href="<?= base_url()?>" title="Indice">Indice</a>
+        <p>Estos son los artículos publicados.</p>
+    </body>
+</html>
+
