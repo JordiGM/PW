a2a8becb96d66fbb923095a55ac70ab386a75ef4
Pablo <Pablo@LAPTOP-UQ3S4M4Q>
2018-11-27 13:17:24 +0100

    Modificación base de datos. Introducción de filas de prueba

--- CentroMail/Analisis/CentroMail.sql
+++ CentroMail/Analisis/CentroMail.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-11-2018 a las 12:15:09
+-- Tiempo de generación: 27-11-2018 a las 13:14:39
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
@@ -35,6 +35,14 @@ CREATE TABLE `calificacioncontenido` (
   `Descripcion` varchar(128) NOT NULL COMMENT 'Descripción de la calificación\n'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Posibles calificaciones de PEGI obtenidas por los juegos para las distintas edades';
 
+--
+-- Volcado de datos para la tabla `calificacioncontenido`
+--
+
+INSERT INTO `calificacioncontenido` (`id`, `Nombre`, `Imagen`, `Descripcion`) VALUES
+(1, 'Lenguaje soez', '', 'En este juego se podrá encontrar lenguaje soez, por lo que no es indicado que lo jueguen menores de edad'),
+(2, 'Discriminación', '', 'En este videojuego se encontrarán escenas en las que se discrimine a un grupo de personas o al propio protagonista');
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +56,14 @@ CREATE TABLE `calificacionedad` (
   `Descripcion` varchar(128) NOT NULL COMMENT 'Descripción de la calificación\n'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Posibles calificaciones de PEGI obtenidas por los juegos para las distintas edades';
 
+--
+-- Volcado de datos para la tabla `calificacionedad`
+--
+
+INSERT INTO `calificacionedad` (`id`, `Nombre`, `Imagen`, `Descripcion`) VALUES
+(1, 'PEGI 3', '', 'Este juego puede jugarlo las personas que tengan 3 o más años'),
+(2, 'PEGI 7', '', 'Este juego puede jugarlo las personas que tengan 7 o más años');
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +74,14 @@ CREATE TABLE `compannia` (
   `id` int(11) NOT NULL,
   `Nombre` varchar(64) NOT NULL COMMENT 'Nombre de la compañia'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Nombre de las compañias productoras de plataformas';
+
+--
+-- Volcado de datos para la tabla `compannia`
+--
+
+INSERT INTO `compannia` (`id`, `Nombre`) VALUES
+(1, 'Sony'),
+(2, 'Microsoft');
 
 -- --------------------------------------------------------
 
@@ -70,6 +94,36 @@ CREATE TABLE `genero` (
   `Nombre` varchar(64) NOT NULL COMMENT 'Nombre del genero',
   `Descripcion` varchar(64) NOT NULL COMMENT 'Descripción del contenido del genero'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Generos de videojuegos';
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`id`, `Nombre`, `Descripcion`) VALUES
+(1, 'Acción', 'Consiste en eliminar enemigos. El contenido va a ser violento'),
+(2, 'Aventura', 'Consiste en recoger objetos de valor. Juegos de larga duración'),
+(3, 'Aventura gráfica', 'El jugador puede interactuar con el escenario y los personajes d'),
+(4, 'Espionaje', 'Lo importante es pasar inadvertido a los enemigos'),
+(5, 'FREE', 'El jugador tiene una gran libertad en sus movimientos y acciones'),
+(6, 'Cartas', 'Son los juegos de cartas de toda la vida, como el Solitario'),
+(7, 'Combate de cartas', 'En este, el jugador intenta eliminar los puntos de vida del cont'),
+(8, 'Acción con cartas', 'Los naipes sirven para seleccionar las acciones a realizar'),
+(9, 'Conducción', 'Conducción de vehículos. Suelen consistir en torneos y campeonat'),
+(10, 'Formula 1', 'Conducción de un Formula 1'),
+(11, 'Karts', 'Conducción de karts'),
+(12, 'Motociclismo', 'Conducción de motocicletas'),
+(13, 'Construcción', 'El objetivo es construir una estructura, mantenerla y gestionarl'),
+(14, 'Cooperación', 'Es necesaria la cooperación con otros personajes'),
+(15, 'Cyberpunk', 'Combinación de ciencia ficción futurista con sociedades distópic'),
+(16, 'Baloncesto', 'Simulación de partidos de baloncesto. Puede incluir modo carrera'),
+(17, 'Deportes olímpicos', 'Replican las pruebas realizadas en las olimpiadas'),
+(18, 'Fútbol', 'Simulación de partidos de fútbol. Puede incluir modo carrera'),
+(19, 'Golf', 'Simulación de partidos de golf. Puede incluir modo carrera'),
+(20, 'Tennis', 'Simulación de partidos de tennis. Puede incluir modos carrera'),
+(21, 'Matamarcianos', 'Se dispara proyectiles para destruir naves o criaturas enemigas'),
+(22, 'FPS', 'Solo es visible el arma. Su objetivo es disparar contra los enem'),
+(23, 'Idiomas', 'Su objetivo es ayudar al aprendizaje de idiomas'),
+(24, 'Estrategia en tiempo real', 'Se utilizan tropas para invadir el terreno del enemigo');
 
 -- --------------------------------------------------------
 
@@ -89,6 +143,14 @@ CREATE TABLE `juego` (
   `Imagen` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabla con la información de los juegos';
 
+--
+-- Volcado de datos para la tabla `juego`
+--
+
+INSERT INTO `juego` (`id`, `Titulo`, `Annio`, `ValoracionMedia`, `Descripcion`, `Genero_id`, `CalificacionEdad_id`, `Productora_id`, `Imagen`) VALUES
+(1, 'Monkey Island', 1990, 0, 'Este videojuego trata sobre las locas aventuras de Guybrush Threepwood', 3, 2, 2, ''),
+(2, 'Rayman 2', 2002, 0, 'La mascota de Ubisoft vuelve a las andadas, y esta vez en una aventura 3D', 2, 2, 1, '');
+
 -- --------------------------------------------------------
 
 --
@@ -99,6 +161,13 @@ CREATE TABLE `juegocalificacioncontenido` (
   `Juego_id` int(11) NOT NULL,
   `CalificacionContenido_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `juegocalificacioncontenido`
+--
+
+INSERT INTO `juegocalificacioncontenido` (`Juego_id`, `CalificacionContenido_id`) VALUES
+(1, 1);
 
 -- --------------------------------------------------------
 
@@ -113,6 +182,13 @@ CREATE TABLE `nick` (
   `Juego_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabla que relaciona los nobres utilizados en el juego por cada usuario';
 
+--
+-- Volcado de datos para la tabla `nick`
+--
+
+INSERT INTO `nick` (`id`, `Nick`, `Usuario_id`, `Juego_id`) VALUES
+(1, 'RaymanLover64', 1, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -125,6 +201,14 @@ CREATE TABLE `plataforma` (
   `Compannia_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Conjunto de plataformas en las que se pueden sacar los juegos\n';
 
+--
+-- Volcado de datos para la tabla `plataforma`
+--
+
+INSERT INTO `plataforma` (`id`, `Nombre`, `Compannia_id`) VALUES
+(1, 'Playstation 4', 1),
+(2, 'Xbox One X', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -136,6 +220,13 @@ CREATE TABLE `plataformajuego` (
   `Juego_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `plataformajuego`
+--
+
+INSERT INTO `plataformajuego` (`Plataforma_id`, `Juego_id`) VALUES
+(1, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -146,6 +237,14 @@ CREATE TABLE `productora` (
   `id` int(11) NOT NULL,
   `Nombre` varchar(64) NOT NULL COMMENT 'Nombre de la plataforma'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Conjunto de productoras en las que se pueden sacar los juegos\n';
+
+--
+-- Volcado de datos para la tabla `productora`
+--
+
+INSERT INTO `productora` (`id`, `Nombre`) VALUES
+(1, 'Ubisoft España'),
+(2, 'Nintendo');
 
 -- --------------------------------------------------------
 
@@ -181,6 +280,14 @@ CREATE TABLE `valoracion` (
   `Usuario_id` int(11) NOT NULL,
   `Juego_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Comentarios y puntuaciones otorgadas por los usuarios';
+
+--
+-- Volcado de datos para la tabla `valoracion`
+--
+
+INSERT INTO `valoracion` (`Puntuacion`, `Comentario`, `Usuario_id`, `Juego_id`) VALUES
+(4, 'Es un poco lioso, pero al final es bastante bueno hasta que no he podido llegar más lejos', 1, 1),
+(5, 'Este es el mejor juego. Me recordó a mi infancia', 1, 2);
 
 --
 -- Índices para tablas volcadas
@@ -278,49 +385,49 @@ ALTER TABLE `valoracion`
 -- AUTO_INCREMENT de la tabla `calificacioncontenido`
 --
 ALTER TABLE `calificacioncontenido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `calificacionedad`
 --
 ALTER TABLE `calificacionedad`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `compannia`
 --
 ALTER TABLE `compannia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `juego`
 --
 ALTER TABLE `juego`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador unico del juego';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador unico del juego', AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `nick`
 --
 ALTER TABLE `nick`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `plataforma`
 --
 ALTER TABLE `plataforma`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `productora`
 --
 ALTER TABLE `productora`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
