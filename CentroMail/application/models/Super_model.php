4cf6e25c3bf8b012f2ebdac78a4decfa36d8fedf
sheratan <sheratan@sheratan-GE70-2PE>
2018-12-04 17:24:39 +0100

    Inegrado
    Usuario
    Calificación Edad
    Calificación Contenido
    Productora
    Genero
    Compañia
    Plataforma

--- CentroMail/application/models/Super_model.php
+++ CentroMail/application/models/Super_model.php
@@ -141,14 +141,17 @@ class Super_model extends CI_Model {
         return $update->result_array();
     }
     
+    
+    
+    
     /**
      *          Metodos de CalificacionesEdad
      */
     
     //Método que devuelve la calificación por edad de un juego
-    public function get_ageCalification ($iIdGame){
+    public function get_ageCalification ($iIdAgeCali){
         $this->db->from('CalificacionEdad');
-        $this->db->where('id', $iIdGame);
+        $this->db->where('id', $iIdAgeCali);
         $consulta = $this->db->get();
         
         return $consulta->result_array();
@@ -156,7 +159,7 @@ class Super_model extends CI_Model {
     
     //Metodo que añade una calificación por edad a la base de datos
     public function add_ageCalification(){
-        $this->db->insert('Usuario', array(
+        $this->db->insert('CalificaciónEdad', array(
             //Utilizamos true para evitar inyecciones xss
             'Nombre'=>$this->input->post('Nombre',TRUE),
             'Imagen'=>$this->input->post('Imagen',TRUE),
@@ -168,32 +171,235 @@ class Super_model extends CI_Model {
     public function set_name_ageCalification($iId, $sName){
         $data = array('Nombre' => $sName);
         $this->db->where('id', $iId);
-        $update = $this->db->update('CalificacionEdad', $data);
-        
-        return $update->result_array();
+        $this->db->update('CalificacionEdad', $data);
     }
     
     //Metodo que modifica el nombre de la calificación por edad
     public function set_img_ageCalification($iId, $sUrl){
         $data = array('Imagen' => $sUrl);
         $this->db->where('id', $iId);
-        $update = $this->db->update('CalificacionEdad', $data);
-        
-        return $update->result_array();
+        $this->db->update('CalificacionEdad', $data);
     }
     
-    //Metodo que modifica el nombre de la calificación por edad
+    //Metodo que modifica la descripción de la calificación por edad
     public function set_description_ageCalification($iId, $sDescription){
         $data = array('Descripcion' => $sDescription);
         $this->db->where('id', $iId);
-        $update = $this->db->update('CalificacionEdad', $data);
-        
-        return $update->result_array();
+        $this->db->update('CalificacionEdad', $data);
     }
     
     //Metodo que elimina la calificación por edad
     public function remove_ageCalification($iId){
         $this->db->where('id', $iId);
         $this->db->delete('CalificacionEdad');
+    }
+    
+    
+    
+    
+    /**
+     *          Metodos de CalificacionesContenido
+     */
+    
+    //Método que devuelve la calificación por contenido de un juego
+    public function get_contentCalification ($iIdContentCali){
+        $this->db->from('CalificacionContenido');
+        $this->db->where('id', $iIdContentCali);
+        $consulta = $this->db->get();
+        
+        return $consulta->result_array();
+    }
+    
+    //Metodo que añade una calificación por contenido a la base de datos
+    public function add_contentCalification(){
+        $this->db->insert('CalificaciónContenido', array(
+            //Utilizamos true para evitar inyecciones xss
+            'Nombre'=>$this->input->post('Nombre',TRUE),
+            'Imagen'=>$this->input->post('Imagen',TRUE),
+            'Descripcion'=>$this->input->post('Descripcion',TRUE),
+        ));
+    }
+    
+    //Metodo que modifica el nombre de la calificación por contenido
+    public function set_name_contentCalification($iId, $sName){
+        $data = array('Nombre' => $sName);
+        $this->db->where('id', $iId);
+        $this->db->update('CalificacionContenido', $data);
+    }
+    
+    //Metodo que modifica el nombre de la calificación por contenido
+    public function set_img_contentCalification($iId, $sUrl){
+        $data = array('Imagen' => $sUrl);
+        $this->db->where('id', $iId);
+        $this->db->update('CalificacionContenido', $data);
+    }
+    
+    //Metodo que modifica la descripción de la calificación por contenido
+    public function set_description_contentCalification($iId, $sDescription){
+        $data = array('Descripcion' => $sDescription);
+        $this->db->where('id', $iId);
+        $this->db->update('CalificacionContenido', $data);
+    }
+    
+    //Metodo que elimina la calificación por contenido
+    public function remove_contentCalification($iId){
+        $this->db->where('id', $iId);
+        $this->db->delete('CalificacionContenido');
+    }
+    
+    
+    
+    
+    /**
+     *          Metodos de Genero
+     */
+    
+    //Método que devuelve la información de un genero
+    public function get_genero ($iIdGenero){
+        $this->db->from('Genero');
+        $this->db->where('id', $iIdGenero);
+        $consulta = $this->db->get();
+        
+        return $consulta->result_array();
+    }
+    
+    //Metodo que añade un genero a la base de datos
+    public function add_genero(){
+        $this->db->insert('Genero', array(
+            //Utilizamos true para evitar inyecciones xss
+            'Nombre'=>$this->input->post('Nombre',TRUE),
+            'Descripcion'=>$this->input->post('Descripcion',TRUE),
+        ));
+    }
+    
+    //Metodo que modifica el nombre del genero
+    public function set_name_genero($iId, $sName){
+        $data = array('Nombre' => $sName);
+        $this->db->where('id', $iId);
+        $this->db->update('Genero', $data);
+    }
+    
+    //Metodo que modifica la drescripción del genero
+    public function set_description_genero($iId, $sDescription){
+        $data = array('Descripcion' => $sDescription);
+        $this->db->where('id', $iId);
+        $this->db->update('Genero', $data);
+    }
+    
+    //Metodo que elimina el genero
+    public function remove_genero($iId){
+        $this->db->where('id', $iId);
+        $this->db->delete('Genero');
+    }
+    
+    
+        
+    
+    /**
+     *          Metodos de Productora
+     */
+    
+    //Método que devuelve la información de una productora
+    public function get_productora ($iIdProductora){
+        $this->db->from('Propductora');
+        $this->db->where('id', $iIdProductora);
+        $consulta = $this->db->get();
+        
+        return $consulta->result_array();
+    }
+    
+    //Metodo que añade una productora a la base de datos
+    public function add_productora(){
+        $this->db->insert('Productora', array(
+            //Utilizamos true para evitar inyecciones xss
+            'Nombre'=>$this->input->post('Nombre',TRUE),
+        ));
+    }
+    
+    //Metodo que modifica el nombre de la productora
+    public function set_name_productora($iId, $sName){
+        $data = array('Nombre' => $sName);
+        $this->db->where('id', $iId);
+        $this->db->update('Productora', $data);
+    }
+    
+    //Metodo que elimina la productora
+    public function remove_productora($iId){
+        $this->db->where('id', $iId);
+        $this->db->delete('Productora');
+    }
+    
+    
+        
+    
+    /**
+     *          Metodos de Compannia
+     */
+    
+    //Método que devuelve la información de una compañia
+    public function get_compannia ($iIdCompannia){
+        $this->db->from('Compannia');
+        $this->db->where('id', $iIdCompannia);
+        $consulta = $this->db->get();
+        
+        return $consulta->result_array();
+    }
+    
+    //Metodo que añade una compañia a la base de datos
+    public function add_compannia(){
+        $this->db->insert('Compannia', array(
+            //Utilizamos true para evitar inyecciones xss
+            'Nombre'=>$this->input->post('Nombre',TRUE),
+        ));
+    }
+    
+    //Metodo que modifica el nombre de la compañia
+    public function set_name_compannia($iId, $sName){
+        $data = array('Nombre' => $sName);
+        $this->db->where('id', $iId);
+        $this->db->update('Compannia', $data);
+    }
+    
+    //Metodo que elimina la compañia
+    public function remove_compannia($iId){
+        $this->db->where('id', $iId);
+        $this->db->delete('Compannia');
+    }
+    
+    
+        
+    
+    /**
+     *          Metodos de Plataforma
+     */
+    
+    //Método que devuelve la información de una plataforma
+    public function get_plataform ($iIdPlataform){
+        $this->db->from('Plataforma');
+        $this->db->where('id', $iIdPlataform);
+        $consulta = $this->db->get();
+        
+        return $consulta->result_array();
+    }
+    
+    //Metodo que añade una plataforma a la base de datos
+    public function add_plataform(){
+        $this->db->insert('Plataforma', array(
+            //Utilizamos true para evitar inyecciones xss
+            'Nombre'=>$this->input->post('Nombre',TRUE),
+        ));
+    }
+    
+    //Metodo que modifica el nombre de la plataforma
+    public function set_name_plataform($iId, $sName){
+        $data = array('Nombre' => $sName);
+        $this->db->where('id', $iId);
+        $this->db->update('Plataforma', $data);
+    }
+    
+    //Metodo que elimina la plataforma
+    public function remove_plataform($iId){
+        $this->db->where('id', $iId);
+        $this->db->delete('Plataforma');
     }
 }
