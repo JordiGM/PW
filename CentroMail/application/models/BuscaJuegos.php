d36a0e1ca33e9f2ba775f619136094edfcc1ed03
sheratan <sheratan@sheratan-GE70-2PE>
2018-12-11 15:20:25 +0100

    Merge origin/master
    Conflicts:
    	CentroMail/application/models/BuscaUsuario.php

--- CentroMail/application/models/BuscaJuegos.php
+++ CentroMail/application/models/BuscaJuegos.php
@@ -5,36 +5,36 @@ class Busqueda_Juego extends CI_Model{
  }
  
  public function busca_nombre($nombre){
+ $ssql= "select * from Juego where titulo=''.$nombre.''order by valoracionMedia";
  $ssql= "select * from Juego where titulo="".$nombre.""order by valoracionMedia";
  $conexion = mysqli_connect("localhost", "root", "", "codeigniter");
  $consulta = mysqli_query($conexion,$ssql);
   return $consulta->result();
  }
   public function busca_valoracion($media){
- $ssql= "select * from Juego where valoracionMedia="".$media.""order by valoracionMedia";
+ $ssql= "select * from Juego where valoracionMedia=''.$media.''order by valoracionMedia";
  $conexion = mysqli_connect("localhost", "root", "", "codeigniter");
  $consulta = mysqli_query($conexion,$ssql);
    return $consulta->result();
  }
  
    public function busca_tipo($tipo){
- $ssql= "select * from Juego where genero="".$tipo.""order by valoracionMedia";
+ $ssql= "select * from Juego where genero=''.$tipo.''order by valoracionMedia";
  $conexion = mysqli_connect("localhost", "root", "", "codeigniter");
  $consulta = mysqli_query($conexion,$ssql);
    return $consulta->result();
  }
     public function busca_edad($edad){
- $ssql= "select * from Juego where calificacionedad="".$edad.""order by valoracionMedia";
+ $ssql= "select * from Juego where calificacionedad=''.$edad.''order by valoracionMedia";
  $conexion = mysqli_connect("localhost", "root", "", "codeigniter");
  $consulta = mysqli_query($conexion,$ssql);
    return $consulta->result();
  }
     public function busca_productora($productora){
- $ssql= "select * from Juego where productora="".$productora.""order by valoracionMedia";
+ $ssql= "select * from Juego where productora=''.$productora.''order by valoracionMedia";
  $conexion = mysqli_connect("localhost", "root", "", "codeigniter");
  $consulta = mysqli_query($conexion,$ssql);
    return $consulta->result();
  }
  
 }
-?>
