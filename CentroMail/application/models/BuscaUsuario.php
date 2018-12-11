e8b03aab5fcd2ab9ec7dd944950d5bf8bde483d0
Pablo <Pablo@LAPTOP-UQ3S4M4Q>
2018-12-11 14:52:07 +0100

    Prueba de insercion.

--- CentroMail/application/models/BuscaUsuario.php
+++ CentroMail/application/models/BuscaUsuario.php
@@ -5,13 +5,13 @@ class Busqueda_Usuario extends CI_Model{
  }
  
  public function busca_nombre($nombre){
- $ssql= "select * from Usuario where nombre="".$nombre.";
+ $ssql= "select * from Usuario where nombre=''.$nombre.";
  $conexion = mysqli_connect("localhost", "root", "", "codeigniter");
  $consulta = mysqli_query($conexion,$ssql);
  return $consulta->result();
  }
   public function busca_nick($nick){
- $ssql= "select * from Usuario,Nick where Usuario.id=Nick.Usuario_id and Nick.nick="".$nick.";
+ $ssql= "select * from Usuario,Nick where Usuario.id=Nick.Usuario_id and Nick.nick=''.$nick.";
  $conexion = mysqli_connect("localhost", "root", "", "codeigniter");
  $consulta = mysqli_query($conexion,$ssql);
  return $consulta->result();
