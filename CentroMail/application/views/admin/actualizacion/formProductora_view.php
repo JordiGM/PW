dfe788a117bb63e27ceeeca16bdc1c9214811053
sheratan <sheratan@sheratan-GE70-2PE>
2018-12-18 09:46:21 +0100

    Actualiza el nombre de una productora

--- CentroMail/application/views/admin/actualizacion/formProductora_view.php
+++ CentroMail/application/views/admin/actualizacion/formProductora_view.php
@@ -7,14 +7,13 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Registro de productora <?php echo $productora[0]['Nombre'] ?></title>
+        <title>Actualización de productora <?php echo $productora[0]['Nombre'] ?></title>
     </head>
     <body>
-        <h1>Registro de productora <?php echo $productora[0]['Nombre'] ?></h1>
-        <?= form_open(base_url() . 'index.php/Admin/verifyRegisterProductora', array('name' => 'form_reg')); ?>
+        <h1>Actualización de productora <?php echo $productora[0]['Nombre'] ?></h1>
+        <?= form_open(base_url() . "index.php/Admin/verifyRegisterProductora/".$productora[0]['id'], array('name' => 'form_reg')); ?>
         <?= form_label('Nombre', 'Nombre'); ?>
         <?= form_input('Nombre', @set_value('Nombre')) ?><br/><br/>
-
         <?= form_submit('submit_reg', 'Registrar'); ?>
 
         <a href="<?= base_url() . 'index.php/Admin/' ?>" title="PanelAdmin">Panel de Administración</a>
